e0c7b025dc7018fd8ace91418ff02808fc50e2d9
AdeptusAstartes <dangelillo@gmail.com>
2022-09-19 18:34:59 -0400

    update package

--- Package.swift
+++ Package.swift
@@ -5,10 +5,10 @@ import PackageDescription
 
 let package = Package(
     name: "AlamofireRSSParser",
-    platforms: [.iOS(.v10),
-                .macOS(.v10_12),
-                .tvOS(.v10),
-                .watchOS(.v3)],
+    platforms: [.iOS(.v11),
+                .macOS(.v10_13),
+                .tvOS(.v11),
+                .watchOS(.v4)],
     products: [.library(name: "AlamofireRSSParser", targets: ["AlamofireRSSParser"])],
     dependencies: [.package(url: "https://github.com/Alamofire/Alamofire.git",
                             from: "5.0.0")],
